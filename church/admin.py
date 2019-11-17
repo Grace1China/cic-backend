1f3c99dc8ccf2f647de873a3340b8d1e5e4c647f
Thinking Song <123wuming_123@163.com>
2019-11-17 21:48:27 +0800

    add admin

--- church/admin.py
+++ church/admin.py
@@ -2,4 +2,15 @@ from django.contrib import admin
 
 from . import models
 
+# class ChurchAdmin(admin.ModelAdmin):
+#     fields = ('name', 'description', 'time_desc', 'address', 'status')
+
+admin.site.register(models.Church)
+admin.site.register(models.Speaker)
+admin.site.register(models.Meeting)
+admin.site.register(models.BibleStudy)
+admin.site.register(models.BibleStudyComment)
+admin.site.register(models.Course)
+admin.site.register(models.Team)
+admin.site.register(models.Donation)
 admin.site.register(models.User)
