a234f2206d17760b3ea20d6a757bf511af97e149
Thinking Song <123wuming_123@163.com>
2019-11-19 20:00:38 +0800

    add STATIC_ROOT

--- church/settings.py
+++ church/settings.py
@@ -128,6 +128,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.AutoSchema'
 }
