69f60fd609940defc39d00b006963b52c6bc7934
Thinking Song <123wuming_123@163.com>
2019-11-30 20:05:07 +0800

    Update urls.py

--- church/urls.py
+++ church/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, re_path, include
 from rest_framework_swagger.views import get_swagger_view
 from . import view
 
-schema_view = get_swagger_view(title='Pastebin API')
+schema_view = get_swagger_view(title='Church API')
 
 urlpatterns = [
     path('hello', view.hello),
