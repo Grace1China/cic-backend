4d6eb4febc3f1fbb3ab98e1bc69c87a2130ca27f
fightorqin@gmail.com <fightorqin@gmail.com>
2020-01-10 23:14:21 +0800

    for dev log

--- church/urls.py
+++ church/urls.py
@@ -28,9 +28,13 @@ schema_view = get_swagger_view(title='Church API')
 urlpatterns = [
     path('hello', view.hello),
     path('admin/', admin.site.urls),
+    path('admindev/', admin.site.urls),
+
     re_path('swagger/', schema_view),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     re_path(r'^admin/ckeditor/', include('ckeditor_uploader.urls')),
+    re_path(r'^admindev/ckeditor/', include('ckeditor_uploader.urls')),
+
 
     # re_path(r'^api/auth/', include('rest_auth.urls')),
     # re_path(r'^api/auth/registration/', include('rest_auth.registration.urls')),
