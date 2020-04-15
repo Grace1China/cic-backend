4c9fc95cccb306c1bae9aff0e3ff57da501dc0df
fightorqin@gmail.com <fightorqin@gmail.com>
2020-04-15 17:43:04 +0800

    media base test 4

---
+++ church/static/church/store.js
@@ -0,0 +1,42 @@
+config = {
+    host:'http://localhost:8000',
+
+}
+async function getImages (context,par) {
+    console.log(`getImages==============path==${par.path}=`)
+    await axios.get(`${config.host}/alioss_list${par.path=='/'?'/':'/'+par.path}`,{params: { 'type': 'images','marker':par.marker }})
+    .then(function (res) {
+        console.log(res)
+        if (res.data.errCode == '0'){
+            context.commit('setImages', res.data.data)
+            // console.log(context.state.menues)
+        }
+    })
+    .catch(function (err) {
+      console.log(err)
+    });
+}
+
+const store = new Vuex.Store({
+    state: {
+      count: 0,
+      images:[],
+
+    },
+    mutations: {
+      increment (state) {
+        state.count++
+      },
+      setImages(state, pimages){
+        state.images = pimages
+      }
+    },
+    actions: {
+      increment (context) {
+        context.commit('increment')
+      },
+     
+      getImages,
+      
+    }
+  })
