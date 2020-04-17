aed5cba97b106645e7c04e65d420b558eff2d892
fightorqin@gmail.com <fightorqin@gmail.com>
2020-04-17 22:52:53 +0800

    test alioss callback in sandbox to json and log it 4

--- api/alioss_directup_views.py
+++ api/alioss_directup_views.py
@@ -179,7 +179,9 @@ class AliOssCallBack(APIView):
             theLogger.info(json.dumps(ret_dict))
             mfile = MediaFile.objects.create(name=ret_dict['filename'], mime_type=ret_dict['mimeType'])
             theLogger.info(mfile)
-            return HttpResponse(json.dumps(ret_dict,ensure_ascii=False),content_type="application/json,charset=utf-8")
+            return Response(data=json.dumps(ret_dict,ensure_ascii=False),status=status.HTTP_200_OK)
+
+            # return HttpResponse(json.dumps(ret_dict,ensure_ascii=False),content_type="application/json,charset=utf-8")
             # retV = JsonResponse(ret_dict, safe=True)
             # theLogger.info(retV.__dict__)
             # return retV
