ef1792bef54945de20935ac28bcefd3cde60c62f
fightorqin@gmail.com <fightorqin@gmail.com>
2020-04-18 09:16:31 +0800

    test callback

--- api/alioss_directup_views.py
+++ api/alioss_directup_views.py
@@ -175,16 +175,17 @@ class AliOssCallBack(APIView):
             ret_dict['signedurl'] = CICUtill.signurl(key=ret_dict['filename'],whichbucket='source')
             ret_dict['String value'] = 'OK'
             ret_dict['Key'] = 'Status'
+            re_dict['Status'] = 'OK'
             theLogger.info(ret_dict)
-            theLogger.info(json.dumps(ret_dict))
+            # theLogger.info(json.dumps(ret_dict))
             mfile = MediaFile.objects.create(name=ret_dict['filename'], mime_type=ret_dict['mimeType'])
             theLogger.info(mfile)
             # return Response(data=json.dumps(ret_dict,ensure_ascii=False),status=status.HTTP_200_OK)
 
-            retV =  HttpResponse(json.dumps(ret_dict,ensure_ascii=False),content_type="application/json,charset=utf-8")
-            # retV = JsonResponse(ret_dict, safe=True)
+            # retV =  HttpResponse(json.dumps(ret_dict,ensure_ascii=False),content_type="application/json,charset=utf-8")
+            retV = JsonResponse({'Status':'OK'}, safe=True)
             theLogger.info(retV.__dict__)
-            return retV
+            return   retV
 
         except Exception as e:
             #import logging
