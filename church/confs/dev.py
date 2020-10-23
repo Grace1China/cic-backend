615d78f01066b24b1206741cf3596f5e8dc338b4
fightorqin@gmail.com <fightorqin@gmail.com>
2020-10-23 18:31:37 +0800

    add parentcol to column 12

--- church/confs/dev.py
+++ church/confs/dev.py
@@ -96,7 +96,10 @@ INSTALLED_APPS += [
 ]
 MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware']
 
-INTERNAL_IPS = ('127.0.0.1',)
+# INTERNAL_IPS = ('127.0.0.1',)
+DEBUG_TOOLBAR_CONFIG = {
+  "JQUERY_URL": '//cdn.bootcss.com/jquery/2.2.4/jquery.min.js',
+}
 
 MAINSITE_API_V1 = 'http://127.0.0.1:8200/mainsite_api_v1/mst/MakeSermon'
 APP_SERVER_IP = "localhost:8000"  # singpore 54.169.143.92  
